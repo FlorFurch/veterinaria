0c9b1d298f55027aa09834ec17a0753d3980642d
FlorFurch <florenciafurch@gmail.com>
2026-04-12 01:07:03 +0000

    primer commit

---
+++ backend/src/django/veterinaria/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+#es donde vamos a escribir las funciones que van a manejar las peticiones HTTP y devolver respuestas HTTP
+# por ejemplo, podemos escribir una función que devuelva un mensaje de bienvenida 
